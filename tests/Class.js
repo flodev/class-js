3a0f1acae600fa3ec885031e40d0bd81c375fc61
Florian <f.biewald@googlemail.com>
2012-01-29 19:56:39 +0100

    added first tests, added gitignore

---
+++ tests/Class.js
@@ -0,0 +1,50 @@
+
+function createTestObj(name) 
+{
+    $.Class(name, {
+        init: function() {
+            console.log("constructor");
+        },
+
+        testFunc: function() {
+            console.log("new func");
+        }
+    });
+}
+
+
+describe('Instantiation tests', function() {
+
+    beforeEach(function() {
+        delete window.TestObj;
+        createTestObj('TestObj');
+    });
+
+    describe('Test prototype related.', function() {
+         it('Should create a function TestObj', function() {
+
+            expect(TestObj).toBeDefined();
+            expect(typeof(TestObj) == 'function').toBeTruthy();
+        });
+
+        it('Should call the prototype constructor on instantiation', function() {
+            
+            sinon.spy(TestObj.prototype, "init");
+            var test = new TestObj();
+            expect(test.init.calledOnce).toBeTruthy();
+        });
+
+        it('Should have defined properties', function() {
+            
+            var test = new TestObj();
+            expect(test.init).toBeDefined();
+            expect(typeof(test.init) == 'function').toBeTruthy();
+
+            expect(test.testFunc).toBeDefined();
+            expect(typeof(test.testFunc) == 'function').toBeTruthy();
+
+        });
+       
+    });
+
+});
